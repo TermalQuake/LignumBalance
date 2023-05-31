47c66f220f968842e24e4f6ebb61945ff208cc4f
Sergey Velikonsky <94070808+TermalQuake@users.noreply.github.com>
2023-06-01 02:20:13 +0300

    add custom craft FIREWORK_ROCKET

--- src/main/java/me/lignumbalance/main.java
+++ src/main/java/me/lignumbalance/main.java
@@ -1,8 +1,21 @@
 package me.lignumbalance;
 
+//main
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.lignumbalance.Listeners.villager;
 import me.lignumbalance.Listeners.elytrafix;
+
+//fireworkFly
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+
+import java.util.Collections;
+
 public final class main extends JavaPlugin {
 
     @Override
@@ -10,6 +23,30 @@ public final class main extends JavaPlugin {
         System.out.println("\u001B[32m" + "All works!" + "\u001B[0m");
         this.getServer().getPluginManager().registerEvents(new villager(), this);
         this.getServer().getPluginManager().registerEvents(new elytrafix(), this);
+
+        // craft balanced firework
+        ItemStack fireworkFly = new ItemStack(Material.FIREWORK_ROCKET);
+
+        NamespacedKey key = new NamespacedKey(this, "my-custom-firework");
+        ItemMeta im = fireworkFly.getItemMeta();
+        FireworkMeta meta = (FireworkMeta) Bukkit.getItemFactory().getItemMeta(Material.FIREWORK_ROCKET);
+
+        meta.setPower(1);
+        im.setDisplayName("Чудо Фейверк");
+        im.setLore(Collections.singletonList("123"));
+        fireworkFly.setItemMeta(im);
+
+        // Set firework power
+        FireworkMeta fireworkMeta = (FireworkMeta) fireworkFly.getItemMeta();
+        fireworkMeta.setPower(1);
+        fireworkFly.setItemMeta(fireworkMeta);
+
+        ShapedRecipe recipe = new ShapedRecipe(key, fireworkFly);
+        recipe.shape("AB");
+        recipe.setIngredient('A', Material.DIAMOND);
+        recipe.setIngredient('B', Material.FIREWORK_ROCKET);
+        // Register craft
+        getServer().addRecipe(recipe);
     }
 
     @Override
@@ -17,3 +54,5 @@ public final class main extends JavaPlugin {
         System.out.println("\u001B[31m" + "Doesn't work!" + "\u001B[0m");
     }
 }
+
+
